3752eaedb05ca2d784a69a6187c1f576bba30793
Vladislav <kelsenelenelvial@mail.ru>
2021-12-27 21:18:58 +0300

    Adaptability styles

--- React Dashboard/src/components/CardsItem/CardsItem.jsx
+++ React Dashboard/src/components/CardsItem/CardsItem.jsx
@@ -20,7 +20,7 @@ export const CardsItem = (props) => {
 			<img src="https://ucare.timepad.ru/599fab4c-51ee-46d6-bafa-0456510ab487/poster_event_1501201.jpg"
 				alt={"img" + id} />
 			<div className="card_body">
-				<p className="card_body_title">${name}</p>
+				<p className="card_body_title">{name}</p>
 
 				<div className="card_body_date">17.11.21</div>
 				<div
@@ -37,7 +37,7 @@ export const CardsItem = (props) => {
 							d="M13.544 0.979025C11.9386 -0.513489 9.55096 -0.245028 8.07735 1.41368L7.50022 2.06246L6.92308 1.41368C5.4524 -0.245028 3.06182 -0.513489 1.45639 0.979025C-0.383422 2.69206 -0.4801 5.76658 1.16635 7.62343L6.83519 14.009C7.20139 14.4212 7.79611 14.4212 8.16231 14.009L13.8311 7.62343C15.4805 5.76658 15.3839 2.69206 13.544 0.979025Z" />
 					</svg>
 				</div>
-				<p className="card_body_text">${body}
+				<p className="card_body_text">{body}
 				</p>
 			</div>
 			<button type="button" className="card_item_button">Read More</button>
